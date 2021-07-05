7e67bdd1166f80e5cefea7e256d066b8c5787092
hj <sqaurelu@ajou.ac.kr>
2021-07-05 23:17:24 +0900

    Add palette and  responsive layout

---
+++ README.md
@@ -0,0 +1,5 @@
+# 그림판
+
+## reference
+
+https://kor.pngtree.com/freepng/vector-color-palette-icon_4190415.html
--- index.js
+++ index.js
@@ -1,10 +1,58 @@
 const canvas = document.getElementById('draw-wrapper');
 
+canvas.width = window.innerWidth * 0.85;
+canvas.height = window.innerHeight * 0.7;
+
+// 색상
+const colors = document.getElementsByClassName('box');
+
+const colorsArray = ['빨간색', '주황색', '노란색', '연두색', '초록색', '하늘색', '파란색', '보라색', '자주색', '흰색', '연회색', '진회색'];
+
+for (let i = 0; i < colors.length; i++) {
+    colors[i].addEventListener('click', (e) => {
+        const color = e.target.style.backgroundColor;
+
+        ctx.strokeStyle = color;
+        console.log('zmfkdmfkdsfsi', e.target.className)
+
+        const snackbar = document.getElementById('snackbar');
+        snackbar.innerText = colorsArray[i] + '으로 색깔이 바꼈습니다.';
+        
+        snackbar.className = 'box show';
+
+        setTimeout(function() { 
+            snackbar.className = snackbar.className.replace("box show", "box"); 
+        }, 3000);
+    });
+}
+
+const toggles = document.querySelectorAll('.toggle'); // NodeList(유사배열)
+const toggleBtn = document.getElementById('toggle-btn');
+
+toggleBtn.addEventListener('click', () => {
+    toggleElements();
+});
+
+function toggleElements() {
+    [].forEach.call(toggles, (toggle) => {
+        toggle.classList.toggle('on');
+    })
+}
+
+
+
+
+window.addEventListener('resize', () => {
+    // console.log(window.innerWidth);
+    canvas.width = window.innerWidth * 0.85;
+    canvas.height = window.innerHeight * 0.7;
+})
+
 const ctx = canvas.getContext('2d');
 ctx.lineWidth = 1; // 4
+ctx.strokeStyle = '#09f';
 
 canvas.addEventListener('mousedown', (mouseEvent) => {
-    console.log(mouseEvent);
     x = mouseEvent.offsetX;
     y = mouseEvent.offsetY;
     drag = true;
@@ -54,4 +102,15 @@ const inputRange = document.getElementById('input-range');
 inputRange.addEventListener('input', (event) => {
     // console.log(event.target.value);
     ctx.lineWidth = event.target.value;
-})
+});
+
+const circle = document.getElementById('circle');
+const square = document.getElementById('square');
+
+circle.onclick = () => {
+    ctx.lineCap = 'round';
+};
+
+square.onclick = () => {
+    ctx.lineCap = 'butt';
+};
